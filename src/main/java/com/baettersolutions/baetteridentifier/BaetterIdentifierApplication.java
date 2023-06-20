1cb5f368b909cb969546823b4d98ab4ac3185f38
Sebastian Otter-Bäck <113764844+ZerodymeAT@users.noreply.github.com>
2023-06-20 09:11:14 +0200

    push

--- src/main/java/com/baettersolutions/baetteridentifier/BaetterIdentifierApplication.java
+++ src/main/java/com/baettersolutions/baetteridentifier/BaetterIdentifierApplication.java
@@ -1,6 +1,6 @@
 package com.baettersolutions.baetteridentifier;
 
-import org.springframework.boot.SpringApplication;
+import com.baettersolutions.baetteridentifier.database.MasterdataMainHandler;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
@@ -8,6 +8,16 @@ import java.io.IOException;
 @SpringBootApplication
 public class BaetterIdentifierApplication {
 
-    public static void main(String[] args)  throws IOException {
-        SpringApplication.run(BaetterIdentifierApplication.class, args);}
+    public static void main(String[] args) throws IOException {
+//        SpringApplication.run(BaetterIdentifierApplication.class, args);}
+
+        String testfilepath = "src/main/resources/importfiles/testfiles/RND_Std_032023_sample_single.xlsx";
+        int sheetnumber = 0;
+        int headlinecolumn = 0;
+
+
+        MasterdataMainHandler.handlingOfMasterdataInput(testfilepath, sheetnumber, headlinecolumn);
+        /*MasterdataMainHandler testdrive = new MasterdataMainHandler();
+        testdrive.handlingOfMasterdataInput(testfilepath,sheetnumber,headlinecolumn);*/
+    }
 }
--- src/main/java/com/baettersolutions/baetteridentifier/database/MasterdataMainHandler.java
+++ src/main/java/com/baettersolutions/baetteridentifier/database/MasterdataMainHandler.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class MasterdataMainHandler{
 
-    public  void handlingOfMasterdataInput(String filePath, int sheetNumber, int lineOfHeadline) throws IOException {
+    public static void handlingOfMasterdataInput(String filePath, int sheetNumber, int lineOfHeadline) throws IOException {
         String jsonFilepath = "src/main/resources/outputfiles/testfiles/testfile.json";
         new GenerateJson().giveConverterTheFile(filePath, sheetNumber, lineOfHeadline);
         new HttpClient().httpClient(jsonFilepath);
