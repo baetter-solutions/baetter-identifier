4357bb342d61a08856af9a480df569fafc9d701b
Sebastian O-B <113764844+ZerodymeAT@users.noreply.github.com>
2023-07-07 11:17:05 +0200

    navbar finished

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,18 @@
 import {NavLink} from 'react-router-dom'
+
 export const Navbar = () => {
 
     return (
-        <nav className="navbar navbar-expand-lg">
-            <div className="container-fluid">
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-            <NavLink className="nav-link active" aria-current="page" to='/'>Home</NavLink>
-            <NavLink className="nav-link" to='/CustomerFile'>Kundendatei</NavLink>
-            <NavLink className="nav-link disabled" to='/Calculation'>Kalkulation</NavLink>
-            <NavLink className="nav-link disabled" to='/Export'>Export</NavLink>
-            <NavLink className="nav-link" to='/ReferenceData'>Stammdaten</NavLink>
-            <NavLink className="nav-link disabled" to='/About'>About</NavLink>
-                    </div>
-                </div>
-            </div>
-        </nav>
+        <ul className="nav nav-tabs navStyle">
+            <li className="nav-item firstitemPad">
+                <NavLink className="nav-link" aria-current="page" to='/'>Home</NavLink>
+            </li>
+            <li className="nav-item"><NavLink className="nav-link" to='/CustomerFile'>Kundendatei</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link disabled" to='/Calculation'>Kalkulation</NavLink>
+            </li>
+            <li className="nav-item"><NavLink className="nav-link disabled" to='/Export'>Export</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to='/ReferenceData'>Stammdaten</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link disabled" to='/About'>About</NavLink></li>
+        </ul>
     )
 }
