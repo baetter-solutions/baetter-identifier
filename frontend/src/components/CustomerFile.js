6112983a16a7c9a19041db72c6711ad9a3716d21
Sebastian O-B <113764844+ZerodymeAT@users.noreply.github.com>
2023-07-07 10:31:02 +0200

    .

--- frontend/src/components/CustomerFile.js
+++ frontend/src/components/CustomerFile.js
@@ -51,72 +51,36 @@ class CustomerFile extends Component {
                     </Dropzone>
                 </div>
                 <div className="divcontent rounded border">
-                    <p>Zu ergänzen: <br/>
-                        und inkl. Pfad+Dateiname, wird ein JSON generiert und in die DB
-                        gespeichert <br/>
-                        Forumlarfelder [ ]
+                    <p>
                     </p>
-
-                    <select className="form-select form-select-lg mb-3" multiple
-                            aria-label=".form-select-lg multiple select example">
+                    {/*
+                <select className="form-select form-select-lg mb-3" multiple aria-label=".form-select-lg multiple select example">
                         <option defaultValue>Open this select menu</option>
-                        <option value="0">A</option>
-                        <option value="1">B</option>
-                        <option value="2">C</option>
-                        <option value="3">D</option>
-                        <option value="4">E</option>
-                        <option value="5">F</option>
-                        <option value="6">G</option>
-                        <option value="7">H</option>
-                        <option value="8">I</option>
-                        <option value="9">J</option>
-                        <option value="10">K</option>
-                        <option value="11">L</option>
-                        <option value="12">M</option>
-                        <option value="13">N</option>
-                        <option value="14">O</option>
-                        <option value="15">P</option>
-                        <option value="16">Q</option>
-                        <option value="17">R</option>
-                        <option value="18">S</option>
-                        <option value="19">T</option>
-                        <option value="20">U</option>
-                        <option value="21">V</option>
-                        <option value="22">W</option>
-                        <option value="23">X</option>
-                        <option value="24">Y</option>
-                        <option value="25">Z</option>
+                        {Array.from(Array(26), (_, index) => {
+                            const letter = String.fromCharCode(65 + index);
+                            return <option key={index} value={index}>{letter}</option>;
+                        })}
                     </select>
+                    */}
                     <form onSubmit={this.handleSubmit}>
-                            <div className="label-container">
-                                <label htmlFor="sheetForWork" className="col-sm-3 col-form-label">Welches
-                                Tabellenblatt?</label>
-                            </div>
-                            <div className="input-container">
-                                <input type="text" className="form-control custInputMaxWidth" id="sheetForWork"
-                                       value={this.state.sheetForWork}
-                                       onChange={this.handleInputChange}/>
-                            </div>
-                        <div className="mb-3 row">
-                            <label htmlFor="custNumber" className="col-sm-3 col-form-label">Kundennummer</label>
-                            <div className="col-sm-9">
-                                <input type="number" className="form-control custInputMaxWidth" id="custNumber"
-                                       value={this.state.custNumber}
-                                       onChange={this.handleInputChange} min="0" max="999999"/>
-                            </div>
+                        <div className="form-container">
+                            <label htmlFor="sheetForWork">Tabellenblatt:</label>
+                            <input type="text" className="form-control custInputWidth" id="sheetForWork"
+                                   value={this.state.sheetForWork} onChange={this.handleInputChange}/>
                         </div>
-                        <div className="mb-3 row">
-                            <label htmlFor="custName" className="col-sm-3 col-form-label">Kundenname</label>
-                            <div className="col-sm-9">
-                                <input type="text" className="form-control custInputMaxWidth" id="custName"
-                                       value={this.state.custName}
-                                       onChange={this.handleInputChange}/>
-                            </div>
+                        <div className="form-container">
+                            <label htmlFor="custNumber">Kundennummer:</label>
+                            <input type="number" className="form-control custInputWidth" id="custNumber"
+                                   value={this.state.custNumber} onChange={this.handleInputChange} min="111111"
+                                   max="999999"/>
                         </div>
-                        <div className="row">
-                            <div className="col-sm-9 offset-sm-3">
-                                <button type="submit" className="btn btn-primary">Submit</button>
-                            </div>
+                        <div className="form-container">
+                            <label htmlFor="custName">Kundenname:</label>
+                            <input type="text" className="form-control custInputWidth" id="custName" value={this.state.custName}
+                                   onChange={this.handleInputChange}/>
+                        </div>
+                        <div className="form-container">
+                            <button type="submit" className="btn btn-primary">Submit</button>
                         </div>
                     </form>
                 </div>
